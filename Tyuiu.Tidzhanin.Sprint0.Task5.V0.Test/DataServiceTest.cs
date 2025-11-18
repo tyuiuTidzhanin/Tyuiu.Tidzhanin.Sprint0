6b685356720d923143924148aafff1f1331e9a9f
Нгаргондо Тиджани <tyuiuTidzhanin@outlook.com>
2025-11-18 22:48:46 +0500

    Task5

---
+++ Tyuiu.Tidzhanin.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.Tidzhanin.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.Tidzhanin.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
