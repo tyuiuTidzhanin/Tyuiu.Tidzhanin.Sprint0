1887bacd4e52c73d13c4e252b204ebffdb6814b4
Нгаргондо Тиджани <tyuiuTidzhanin@outlook.com>
2025-11-18 22:29:51 +0500

    Task1

---
+++ Tyuiu.Tidzhanin.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello, World!");
+string name = "Тиджани";
+int age = 20;
+Console.WriteLine("Здраствуйте меня зовут: " + $"{name}" + " мне:" + $"{age}" + " год");
+Console.ReadKey();
