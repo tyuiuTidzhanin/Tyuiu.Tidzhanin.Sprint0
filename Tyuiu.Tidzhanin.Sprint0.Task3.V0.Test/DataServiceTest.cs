2637899ec473a59acd5ca1ea4f94a1bad7ac13c3
Нгаргондо Тиджани <tyuiuTidzhanin@outlook.com>
2025-11-18 22:42:35 +0500

    Task3

---
+++ Tyuiu.Tidzhanin.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.Tidzhanin.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.Tidzhanin.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var number = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(number);
+            Assert.AreEqual(15, res);
+        }
+        [TestMethod]
+        public void CheckSubstractionArrayValid()
+        {
+            var number = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubtractionArray(number);
+            Assert.AreEqual(-15, res);
+        }
+        [TestMethod]
+        public void CheckMultArrayValid()
+        {
+            var number = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(number);
+            Assert.AreEqual(120, res);
+        }
+
+    }
+}
