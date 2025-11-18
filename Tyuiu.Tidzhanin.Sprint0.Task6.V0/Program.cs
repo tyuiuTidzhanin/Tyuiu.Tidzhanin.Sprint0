a7b4089cdc6535c97791d82a6e3a6d95492cf3e4
Нгаргондо Тиджани <tyuiuTidzhanin@outlook.com>
2025-11-18 22:51:37 +0500

    Task6

---
+++ Tyuiu.Tidzhanin.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,7 @@
+﻿using Tyuiu.Tidzhanin.Sprint0.Task6.V0.Lib;
+
+int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+Console.WriteLine("Сумма элементов массива =" + DataService.AdditionArray(arraynums));
+Console.WriteLine("Разность элементов массива =" + DataService.SubtractionArray(arraynums));
+Console.WriteLine("Произведение элементов массива =" + DataService.MultiplicationArray(arraynums));
+Console.ReadKey();
