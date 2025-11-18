2387b29962c3fc680e5639b3e8f62477a9d45b4e
Нгаргондо Тиджани <tyuiuTidzhanin@outlook.com>
2025-11-18 22:45:54 +0500

    Task4

---
+++ Tyuiu.Tidzhanin.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using Tyuiu.Tidzhanin.Sprint0.Task4.V0.Lib;
+
+Console.WriteLine(DataService.Addition(1, 5));
+Console.WriteLine(DataService.Subtraction(15, 5));
+Console.WriteLine(DataService.Multiplication(10, 10));
+Console.WriteLine(DataService.Division(5, 5));
+Console.ReadKey();
